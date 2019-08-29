49399f36cfc40c3da6b14d52e60c1b7315bd18ef
gyrii84@gmail.com <gyrii84@gmail.com>
2019-08-29 23:03:24 +0300

    init db make @db

--- app.rb
+++ app.rb
@@ -2,6 +2,17 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+#db init
+def init_db
+	@db = SQLite3::DataBase.new 'leprozorium.db'
+	@db.resusts_as_hash = true
+end
+
+before do
+
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -15,8 +26,8 @@ end
 
 post '/new' do
 
-	@content = params[:content] #add name of form how to call the textarea
+	content = params[:content] #add name of form how to call the textarea
 
 
-  	erb "You typed #{@content}"
+  	erb "You typed #{content}"
 end
