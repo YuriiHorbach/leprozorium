196e2a28c55ce3ad59754f88f7def1708b5d25f4
gyrii84@gmail.com <gyrii84@gmail.com>
2019-08-30 00:26:48 +0300

    add /details/ handler

--- app.rb
+++ app.rb
@@ -70,3 +70,11 @@ post '/new' do
 
   	erb "You typed #{content}"
 end
+
+#output info about post
+get '/details/:post_id' do
+	post_id = params[:post_id]
+
+	erb "Displaying information for post with id #{post_id}"
+
+end
